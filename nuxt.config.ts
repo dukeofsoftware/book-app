a70d90387b46c1a1b1887048fbc46f997d53805e
DukeEmree <kozanfurkanemre@gmail.com>
2022-09-19 19:15:02 +0300

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  target: 'static',
+  router: {
+    base: '/book-app/'
+  },
   css:["assets/css/tailwind.css"],
   build: {
     postcss: {
@@ -10,6 +14,7 @@ export default defineNuxtConfig({
         },
       }
     },
+    
   }
   
 });
