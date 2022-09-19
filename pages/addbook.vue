6fdfc83ddc2c219589910759837316980f86ae81
DukeEmree <kozanfurkanemre@gmail.com>
2022-09-19 20:01:23 +0300

    a

--- pages/addbook.vue
+++ pages/addbook.vue
@@ -6,7 +6,7 @@
               <input type="text" class="m-3 p-3 border-2 shadow-sm" id="kitap" v-model="name" placeholder="Kitap İsmi" />
               <label for="sayfa" class=" flex px-3 mx-3 text-lg align-center justify-center">Sayfa</label>
               <input type="text" id="sayfa" class="m-3 p-3 border-2 shadow-sm" v-model="pages" placeholder="Sayfa Sayısı" />
-              <NuxtLink  to="/" @click="ekle"  class=" text-center  bg-blue-500  duration-150 ease-in-out rounded hover:bg-blue-700 text-white p-3 m-3 text-lg border-2 border-blue-300">Ekle</NuxtLink>
+              <button @click="ekle"  class=" text-center  bg-blue-500  duration-150 ease-in-out rounded hover:bg-blue-700 text-white p-3 m-3 text-lg border-2 border-blue-300">Ekle</button>
             </div>
         </div>
 
