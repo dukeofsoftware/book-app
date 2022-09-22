931ec3764d13f4953e3454f7f78e74017539dd0b
DukeEmree <kozanfurkanemre@gmail.com>
2022-09-22 20:46:20 +0300

    a

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
-  router: {
-    base: '/duke-book/'
-  },
+
   css:["assets/css/tailwind.css"],
   build: {
     postcss: {
